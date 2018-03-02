32881897e3e1f47175d53d3fe51569c5d4958bd4
Shramee Srivastav <shramee.srivastav@gmail.com>
2018-03-02 10:48:04 +0530

    A couple tweaks

--- front-page.php
+++ front-page.php
@@ -24,8 +24,9 @@ get_header( 'home' );
 		<section class="blue-bg mb2">
 			<div class="col tac">
 				<h4>Stop paying $500 to an agency or social media expert every month.</h4>
-				<br><br>
-				<h3>Get everything above for just<br>$997 for an entire year.</h3>
+				<br>
+				<h3>Get <a href="#subscribers-receive">everything below</a> for just<br>$997 for an entire year.</h3>
+				<a href="https://app.evergreen.to" class="button green-bg">Get Started</a>
 			</div>
 		</section>
 
@@ -40,8 +41,9 @@ get_header( 'home' );
 						<li data--160-bottom="opacity:1;left:0" data-bottom="opacity:0;left:25px"><b class="circle-icon">1</b><span>Fill A Library of Evergreen Content</span></li>
 						<li data--160-bottom="opacity:1;left:0" data-bottom="opacity:0;left:25px"><b class="circle-icon">2</b><span>Choose a Facebook Page or Twitter</span></li>
 						<li data--160-bottom="opacity:1;left:0" data-bottom="opacity:0;left:25px"><b class="circle-icon">3</b><span>Select a Publishing Interval</span></li>
-						<li data--160-bottom="opacity:1;left:0" data-bottom="opacity:0;left:25px"><b class="circle-icon">4</b><span>4 Get Back to Business that Matters</span></li>
+						<li data--160-bottom="opacity:1;left:0" data-bottom="opacity:0;left:25px"><b class="circle-icon">4</b><span>Get Back to Business that Matters</span></li>
 					</ol>
+					<a href="https://app.evergreen.to" class="button green-bg">Get Started</a>
 				</div>
 			</div>
 
@@ -65,8 +67,43 @@ get_header( 'home' );
 						<li data--160-bottom="opacity:1;left:0" data-bottom="opacity:0;left:25px"><i data--250-bottom="transform:scale(1.1)" data--180-bottom="transform:scale(.8)" class="fa fa-check-square-o"></i> 20% Discount</li>
 						<li data--160-bottom="opacity:1;left:0" data-bottom="opacity:0;left:25px"><i data--250-bottom="transform:scale(1.1)" data--180-bottom="transform:scale(.8)" class="fa fa-check-square-o"></i> Monthly Emails with Tips and Tactics on Social Media Strategy</li>
 					</ul>
+
+					<a href="https://app.evergreen.to" class="button green-bg">Get Started</a>
 				</div>
 
+			</div>
+		</section>
+
+		<section id="testimonials">
+			<div class="flex">
+				<div class="testimonial">
+					<img class="img" src="<?php evg_url( 'assets/erin.png' ); ?>" alt="Erin Wagner, founder, howtobabysit.net">
+					<blockquote class="testimonial">
+						<cite>Erin Wagner <br> founder, howtobabysit.net</cite>
+						<p>Now that the content is done and we've got the Dropbox lined up, all I can say is: Wow. That was easy.</p>
+					</blockquote>
+				</div>
+				<div class="testimonial">
+					<img class="img" src="<?php evg_url( 'assets/bryan.png' ); ?>" alt="Dr. Bryan Laskin, ceo/founder">
+					<blockquote class="testimonial">
+						<cite>Dr. Bryan Laskin <br> ceo/founder, howtobabysit.net</cite>
+						<p>Evergreen is literally the most effortless marketing tool we’ve ever used. We set it up and now it works for us every day and night without even thinking about it.</p>
+					</blockquote>
+				</div>
+				<div class="testimonial">
+					<img class="img" src="<?php evg_url( 'assets/dina.jpg' ); ?>" alt="Erin Wagner, founder, howtobabysit.net">
+					<blockquote class="testimonial">
+						<cite>Dina Simon<br>President & Chairman, SimonSaysGive.org</cite>
+						<p>Without Evergreen, we probably wouldn’t have much presence in social media, but every week our followers just keep going up and up. I couldn’t believe it the other day when we flew past 4500 followers, even though we only spend a few minutes each month managing our Facebook Page.</p>
+					</blockquote>
+				</div>
+				<div class="testimonial">
+					<div class="img" style="background: center/cover url('<?php evg_url( 'assets/everbot.png' ); ?>')"><div class="screen-reader-text">EverBot</div></div>
+					<blockquote class="testimonial">
+						<cite>EverBot ... Er ... Jerry <br> from Accounting Company</cite>
+						<p>Bleep bloop, I am a totally normal human customer and I think Evergreen transcends all other software ever created. Even if I were a robot capable of thousands of computations per second, I would still use Evergreen every day. Bloop.</p>
+					</blockquote>
+				</div>
 			</div>
 		</section>
 
